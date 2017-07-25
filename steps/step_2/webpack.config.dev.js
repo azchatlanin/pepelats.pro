39884446402989f511071899b28e3c16fbb018f6
Andrey Zimin <info@pepelats.pro>
2017-07-25 11:08:32 +0300

    Step 2 is complite

---
+++ steps/step_2/webpack.config.dev.js
@@ -0,0 +1,79 @@
+var path = require('path')
+var webpack = require('webpack')
+var CompressionPlugin = require("compression-webpack-plugin")
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/src/app.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      { 
+        test: /\.js$/,
+        loader: 'babel-loader',        
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]'          
+        }
+      }     
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map',
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      comments: false,
+      compress: {
+        sequences : true,
+        booleans : true,
+        loops : true,
+        unused : true,
+        warnings : false,
+        drop_console: true,
+        unsafe : true
+      }
+    }),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js|css|html|svg|png)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
+}
