d482fbb9acb32cc5360283b1de0ed843b93ed7f6
Andrey Zimin <info@pepelats.pro>
2017-07-25 20:34:55 +0300

    Step 3 is complite

---
+++ steps/step_3/server/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(bodyParser.json())
+app.use('/dist', express.static('dist'))
+
+import webpack from 'webpack'                               
+import webpackMiddleware from 'webpack-dev-middleware'      
+import webpackHotMiddleware from 'webpack-hot-middleware'   
+import webpackConfig from '../webpack.config.dev'           
+ 
+if (process.env.NODE_ENV === 'development') {
+  const compiler = webpack(webpackConfig)                     
+  app.use(webpackMiddleware(compiler, {                       
+    hot: true,                                                
+    publicPath: webpackConfig.output.publicPath,              
+    noInfo: true                                              
+  }))                                                         
+  app.use(webpackHotMiddleware(compiler)) 
+}
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.listen(port, () => console.log('Server listen on port =', port, 'ENV =', process.env.NODE_ENV))
