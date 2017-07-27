0443cac7b9ebf4c56c2e54a0bd65ba4cf118bed7
Andrey Zimin <info@pepelats.pro>
2017-07-27 14:00:21 +0300

    Step 4 complite

---
+++ steps/step_4/src/pages/404.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>
+      404
+    </p>    
+    <p>
+      404
+    </p>    
+    <p>
+      404
+    </p>    
+    <p>
+      404
+    </p>    
+    <p>
+      404
+    </p>    
+    <p>
+      404
+    </p>    
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
