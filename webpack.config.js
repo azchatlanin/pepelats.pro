322145fbabb2becb211d3fb63486c9e6eddff363
Andrey Zimin <info@pepelats.pro>
2017-07-27 23:03:16 +0300

    Deploy in Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name: 'img/[name].[ext]?[hash]'
         }
       }
     ]
