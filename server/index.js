f85e26709274801f941371b0020c238733afeeb6
Andrey Zimin <info@pepelats.pro>
2017-07-30 11:51:53 +0300

    15. VueJS. Устанавливаем и настраиваем ESLINT

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import path from 'path'
-import Debug from 'debug'
-import devOptions from './controllers/config/devOptions'
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import Debug from 'debug';
+import devOptions from './controllers/config/devOptions';
 
-const debug = Debug('server:app')
-const port = process.env.PORT || 5000
-const app = express()
+const debug = Debug('server:app');
+const port = process.env.PORT || 5000;
+const app = express();
 
-app.use(bodyParser.json())
-app.use('/dist', express.static('dist'))
+app.use(bodyParser.json());
+app.use('/dist', express.static('dist'));
 
-devOptions(app)
+devOptions(app);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'))
-})
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 
-app.listen(port, () => debug('Server listen on port =', port, 'ENV =', process.env.NODE_ENV))
+app.listen(port, () => debug('Server listen on port =', port, 'ENV =', process.env.NODE_ENV));
