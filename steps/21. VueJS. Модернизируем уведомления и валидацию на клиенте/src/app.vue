08df5151a431b90a3505518ca46c59593db610fa
Andrey Zimin <info@pepelats.pro>
2017-08-05 16:16:14 +0300

    21. VueJS. Модернизируем уведомления и валидацию на клиенте

---
+++ steps/21. VueJS. Модернизируем уведомления и валидацию на клиенте/src/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <top-menu v-show="currentRoute !== '/'"></top-menu>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import topMenu from './components/topMenu.vue'
+import { router } from './app'
+
+export default {
+  data() {
+    return {
+      currentRoute: router.currentRoute.path
+    }
+  },
+  components: {
+    topMenu
+  },
+  watch: {
+    '$route' (to, from) {
+      this.currentRoute = to.path
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
