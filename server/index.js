c07964275c68d9e62e6a6777d531c771b91727a9
Andrey Zimin <zimin6077@gmail.com>
2017-08-17 14:54:28 +0300

    Deploy in Heroku

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ import session from 'express-session'
 import KnexSessionStore from 'connect-session-knex'
 import db from './controllers/config/knex'
 import devOptions from './controllers/config/dev.serv.opt'
+import history from 'connect-history-api-fallback'
+var config = require('../build/config')
 
 import auth from './routes/auth'
 
@@ -21,8 +23,10 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(morgan('dev'))
+app.use(history())
 
-app.use('/dist', express.static('dist'))
+var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
+app.use(staticPath, express.static('./static'))
 
 app.use(session({
   secret: 'secret',
@@ -37,7 +41,7 @@ devOptions(app)
 app.use('/api/auth', auth)
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'))
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
 app.listen(port, () => debug('Server listen on port =', port, 'ENV =', process.env.NODE_ENV))
