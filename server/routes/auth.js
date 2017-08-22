6430fc332a1cedef88c36c3ce3bd7021de254802
Andrey Zimin <zimin6077@gmail.com>
2017-08-22 15:46:16 +0300

    Add validation filder and validation in server side

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import Debug from 'debug'
+import AuthValid from '../modules/validators'
 
 const auth = express.Router()
 const debug = Debug('server:auth')
@@ -7,6 +8,11 @@ const debug = Debug('server:auth')
 auth.post('/', (req, res) => {
   const { email, password } = req.body
   debug(email, password)
+
+  if (!AuthValid.isValid) {
+    res.status(401).json(AuthValid.errors)
+    return
+  }
 })
 
 export default auth
